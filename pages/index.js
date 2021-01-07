4531123a16676e065f16484a4a9b79c8bb08927a
Kevin Tian <ktian0257@gmail.com>
2021-01-06 18:07:02 -0800

    cleaning up for public build

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Smartersaurus</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
@@ -18,33 +17,6 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          {/* <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
           <input></input>
           <a className="button">Submit</a>
         </div>
